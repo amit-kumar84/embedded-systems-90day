02519990509a143a030525d0251408a0be6316da
Amit Kumar <rajputdevloper@gmail.com>
2025-11-21 23:04:04 +0530

    save

---
+++ day_1/hello_world.c
@@ -0,0 +1,19 @@
+/* 
+ * File: hello_world.c
+ * --------------------
+ * This program prints a greeting message to the console.
+ * It serves as a simple introduction to embedded systems programming.
+ */
+// Include Input/Output library
+//printf needs this library
+#include <stdio.h>
+
+//Main function - program start here
+// "int" means it returns integer
+int main(){
+    //print to sceen
+    // \n = new line
+    printf("Hello World, This is Amit's Embededded System Jurney\n");
+    // Return 0 = program ended successfully
+    return 0;
+}
