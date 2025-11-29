11fbbb86637066cc5a0ce32bae63c425e47bb3f9
Amit Kumar <rajputdevloper@gmail.com>
2025-11-29 23:46:08 +0530

    Refactor code structure for improved readability and maintainability

--- day_3/program3_guess_game.c
+++ day_3/program3_guess_game.c
@@ -17,7 +17,31 @@ int main() {
     while (1) { // Infinite loop until the correct guess
         printf("Enter your guess: ");
         scanf("%d", &guess);
-        attempts
+        attempts++;
+
+        if(guess < secret_number) {
+            printf("Too low! Try again.\n\n");
+        } else if(guess > secret_number) {
+            printf("Too high! Try again.\n\n");
+        } else {
+            // Correct guess
+            printf("Congratulations! 🎉\n");
+            printf("You've guessed it! The number was %d.\n", secret_number);
+            printf("It took you %d attempts to guess the number.\n", attempts);
+
+            // Rating based on attempts
+            if (attempts <= 5) {
+                printf("Excellent work! You're a guessing master! 🌟");
+            } else if (attempts <= 10) {
+                printf("Great job! You have a sharp mind! 👍");
+        } else {
+            printf("Good effort! Keep practicing to improve! 💪");
+
+        }
+        break; // Exit the loop after correct guess
+        
+        }
 
     }
+    return 0;
 }
